a715e98f78ea72ac6fdb42000e4910f94f0cd23f
David Sturgill <dbsturgi@ncsu.edu>
2022-07-28 10:36:54 -0400

    Project 1 and exercise 2.

---
+++ proj/p1/trig.c
@@ -0,0 +1,39 @@
+/**
+  */
+#include <stdio.h>
+#include <stdlib.h>
+
+/** Approximation of PI.  For now, we're using our own constant for
+    this. There's also one provided by the gnu math library, once we
+    learn about that. */
+#define PI 3.14159265358979323846
+
+/**
+*/
+double getAngle()
+{
+}
+
+/**
+*/
+double difference( double a, double b )
+{
+}
+
+/**
+*/
+void tableHeader()
+{
+}
+
+/** 
+*/
+void tableRow( int terms, double s, double c, double t )
+{
+}
+
+/**
+  */
+int main()
+{
+}
